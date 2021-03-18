a81bb09e987713c9c1e0885856d1072b341b7043
Rishi P <rishipandey.programming@gmail.com>
2021-03-18 23:22:21 +0530

    added dmt click

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ password.send_keys("Sridhar1973#")
 login_btn = driver.find_element_by_xpath('//*[@id="loginSubmit"]/button')
 login_btn.click()
 
-time.sleep(0.5)
+time.sleep(0.7)
 
 pin = driver.find_element_by_xpath('/html/body/div[3]/fieldset/input')
 pin.send_keys("1234")
@@ -23,7 +23,7 @@ pin.send_keys("1234")
 ok_btn = driver.find_element_by_xpath('/html/body/div[3]/div[7]/div/button')
 ok_btn.click()
 
-time.sleep(0.5)
+time.sleep(0.7)
 
 dmt_btn = driver.find_element_by_xpath('//*[@id="dashboardArea"]/div/div[1]/div[4]/a/div/div/span[1]')
 dmt_btn.click()
