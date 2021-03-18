d8f369a80f118102dedfbf3c0b73c6a4a18e5aab
Rishi P <rishipandey.programming@gmail.com>
2021-03-18 23:12:04 +0530

    added dmt click

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import time
 chrome_driver = "chromedriver\chromedriver.exe"
 driver = webdriver.Chrome(chrome_driver)
 driver.get("https://newpay.co.in/login.php")
-driver.maximize_window()
 user_name = driver.find_element_by_xpath('//*[@id="userid"]')
 user_name.send_keys("RT1760")
 
@@ -16,12 +15,20 @@ password.send_keys("Sridhar1973#")
 login_btn = driver.find_element_by_xpath('//*[@id="loginSubmit"]/button')
 login_btn.click()
 
-time.sleep(0.8)
-
+time.sleep(0.5)
 
 pin = driver.find_element_by_xpath('/html/body/div[3]/fieldset/input')
 pin.send_keys("1234")
 
-
 ok_btn = driver.find_element_by_xpath('/html/body/div[3]/div[7]/div/button')
 ok_btn.click()
+
+time.sleep(0.5)
+
+dmt_btn = driver.find_element_by_xpath('//*[@id="dashboardArea"]/div/div[1]/div[4]/a/div/div/span[1]')
+dmt_btn.click()
+
+driver.maximize_window()
+
+click_mobile_input = driver.find_element_by_xpath('/html/body/div/div[2]/div[2]/div/div[2]/div/div/form/div[1]/div/div/input')
+click_mobile_input.click()
